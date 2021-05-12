22d8ec7f227cc3b256589d94b99c0c10eb5ace66
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-12 17:11:39 +0000

    all

--- monty.h
+++ monty.h
@@ -40,7 +40,7 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number, char *arg);
 void _pall(stack_t **head, unsigned int line_number);
 void treat_line(stack_t **head, char *line);
-void _pint(stack_t **head, unsigned int line_number)
+void _pint(stack_t **head, unsigned int line_number);
 
 
 #endif
